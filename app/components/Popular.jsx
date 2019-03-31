d3d7912ca3a33d1a0d480cb6c9ebcac92128040a
Eugene <marteau9292@gmail.com>
2019-03-31 17:39:21 +0300

    Fetch all repos

--- app/components/Popular.jsx
+++ app/components/Popular.jsx
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
+import api from '../utils/api';
 
 const SelectLanguage = ({ selectedLanguage, onLanguageSelection }) => {
   const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -24,13 +25,48 @@ SelectLanguage.propTypes = {
   onLanguageSelection: PropTypes.func.isRequired
 };
 
-class Popular extends Component {
-  state = { selectedLanguage: 'All' };
+const ReposGrid = ({ repos }) => (
+  <div className="repos-grid">
+    {repos.map((repo, index) => (
+      <div className="repos-grid__item" key={repo.id}>
+        <span className="repos-grid__rank">#{index + 1}</span>
+        <img
+          src={repo.owner.avatar_url}
+          alt="Avatar"
+          width="150"
+          className="repos-grid__avatar"
+        />
+        <a href={repo.html_url} target={repo.name} className="repos-grid__name">
+          {repo.name}
+        </a>
+        <span className="repos-grid__username">@{repo.owner.login}</span>
+        <span className="repos-grid__stars">{repo.stargazers_count} stars</span>
+      </div>
+    ))}
+  </div>
+);
 
-  handleLanguageSelection = lang => this.setState({ selectedLanguage: lang });
+ReposGrid.propTypes = {
+  repos: PropTypes.arrayOf(PropTypes.object)
+};
+
+class Popular extends Component {
+  state = { selectedLanguage: 'All', repos: null };
+
+  handleLanguageSelection = lang => {
+    this.setState({ selectedLanguage: lang, repos: null });
+
+    api
+      .fetchAllRepos(this.state.selectedLanguage)
+      .then(repos => this.setState({ repos }));
+  };
+
+  componentDidMount() {
+    this.handleLanguageSelection(this.state.selectedLanguage);
+  }
 
   render() {
-    const { selectedLanguage } = this.state;
+    const { selectedLanguage, repos } = this.state;
 
     return (
       <Fragment>
@@ -38,9 +74,9 @@ class Popular extends Component {
           selectedLanguage={selectedLanguage}
           onLanguageSelection={this.handleLanguageSelection}
         />
+        {repos ? <ReposGrid repos={repos} /> : 'Loading...'}
       </Fragment>
     );
   }
 }
-
 export default Popular;
